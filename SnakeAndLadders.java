fcebf8122822c26d45b8f6a5e449f5c0879a34b1
Dipesh Walte <dipesh4edutopia@users.noreply.github.com>
2021-01-29 15:32:22 +0530

    [Dipesh] Add AddedSnakeAndLadders

---
+++ SnakeAndLadders.java
@@ -0,0 +1,8 @@
+public class SnakeAndLadders{
+	public static void main(String args[])
+	{
+	System.out.println("Welcome To Snake and Ladders");
+	int position=0;
+	System.out.println("Current Player Position :"+position);
+	}
+}
