cfa3173f421d540d0a43e4615eb73a3bdc83d9c1
Dipesh Walte <dipesh4edutopia@users.noreply.github.com>
2021-01-29 16:16:44 +0530

    [Dipesh] modify AddedSwitchCase

--- SnakeAndLadders.java
+++ SnakeAndLadders.java
@@ -1,9 +1,32 @@
-public class SnakeAndLadders{
-	public static void main(String args[])
-	{
-	System.out.println("Welcome To Snake and Ladders");
-	int position=0;
-	int diceThrow = (int)(Math.ceil(Math.random()*10)%6)+1;
-	System.out.println("Dice Face :"+diceThrow);
+public class SnakeAndLadders {
+	//Static Constants
+	static final int NO_PLAY=0;
+	static final int LADDER=1;
+	static final int SNAKE=2;
+	
+	public static void main(String[] args) {
+		
+		int diceThrow = (int)(Math.ceil(Math.random()*10)%6)+1;
+		int option = (int)(Math.ceil(Math.random()*10)%3);
+		int position = 0;
+		
+		switch (option) {
+			case NO_PLAY:
+				
+				break;
+			case LADDER:
+				position += diceThrow;
+				break;
+			case SNAKE:
+				position -= diceThrow;
+				break;
+			default:
+				break;
+		}
+		
+		System.out.println("Player moved to position: "+position);
 	}
+
 }
+
+
